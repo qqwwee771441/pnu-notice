f3e2d96890c0c2555ed68020ced79b0fff58cab4
dib3474 <dib3474@gmail.com>
2023-01-10 21:20:36 +0900

    add server api

--- source/BackEnd/temporary.py
+++ source/BackEnd/temporary.py
@@ -1,1 +1,15 @@
 # Write Flutter Code
+from flask import Flask, request, json, jsonify
+from cse_notice import get_notice
+app = Flask(__name__)
+
+@app.route("/test", methods=['POST'])
+def test():
+    params = request.get_json()
+    print("받은 Json 데이터 ", params)
+
+    response = get_notice()
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
